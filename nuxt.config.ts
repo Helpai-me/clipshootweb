07f592efa3dc0dd00af2cf6584611e79d5684386
Helpai <helpaimenow@gmail.com>
2026-01-15 12:45:25 +0100

    Set logo.png as favicon and update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,7 @@ export default defineNuxtConfig({
         { name: 'description', content: 'Create stunning app mockups in seconds with ClipShoot.' }
       ],
       link: [
+        { rel: 'icon', type: 'image/png', href: '/logo.png' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap' }
